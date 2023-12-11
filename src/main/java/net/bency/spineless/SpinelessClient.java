28088dcab5b54d6c942184e21e01a969b8d798ab
bency <bencambridge23@gmail.com>
2023-12-11 20:44:59 +0000

    Just trying to make sure the initial commit goes through fully!

---
+++ src/main/java/net/bency/spineless/SpinelessClient.java
@@ -0,0 +1,10 @@
+package net.bency.spineless;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpinelessClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
